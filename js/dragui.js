18e96e246650d29fe670ff7e15b37a44461c3982
Jono Xia <jono@mozilla.com>
2012-02-14 16:34:06 -0800

    Added boxes that show what variable has been assigned to each role; made URL fragment change to reflect current variable assignments.

--- js/dragui.js
+++ js/dragui.js
@@ -1,11 +1,47 @@
 
+var params = {};
+var assignments = {};
+
+function updateFragment() {
+  var baseLoc = ("" + window.location).split("#")[0];
+  var args = [];
+  for (var key in params) {
+    args.push( key + "=" + params[key]);
+  }
+  window.location = baseLoc + "#" + args.join("&");
+}
+
 function initDragGui(){
   var items = $("#variables_menu").find("li");
-  items.draggable({revert: "valid"});
+  items.draggable({opacity: 0.7,
+                   helper: "clone" });
 
   $( ".dragtarget" ).droppable({
     drop: function( event, ui ) {
-      $("#output").html("You dropped " + ui.draggable.html() + " onto " + $(this).attr("id"));
+      var variableName = ui.draggable.html();
+      var variableId = ui.draggable.attr("id");
+      var variableRole = $(this).attr("id");
+
+      var oldRole = assignments[variableName];
+      if (oldRole) {
+        // don't let same variable be in two places - if this variable is already used
+        // somewhere, then swap them (put new role's variable into old role):
+        var varInNewRole = $(this).find(".valbox").html();
+        $("#" + oldRole).find(".valbox").html(varInNewRole);
+        assignments[varInNewRole] = oldRole;
+        params[oldRole] = params[variableRole];
+      }
+
+      $(this).find(".valbox").html(variableName);
+      assignments[variableName] = variableRole;
+      params[variableRole] = variableId;
+
+
+      $("#imagearea").slideUp().slideDown();
+
+      console.log(JSON.stringify(params));
+
+      updateFragment();
     }
   });
 }
